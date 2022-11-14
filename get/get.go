3bb3244af1f33bcff29f4138c17c0f5c11e2e07c
Luca Sepe <luca.sepe@gmail.com>
2022-11-14 22:54:07 +0100

    add resources get method

---
+++ get/get.go
@@ -0,0 +1,59 @@
+package kube
+
+import (
+	kubeutil "github.com/lucasepe/kube/util"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+type GetOpts struct {
+	ChunkSize int64
+
+	LabelSelector string
+	FieldSelector string
+	AllNamespaces bool
+	Namespace     string
+	Subresource   string
+
+	IgnoreNotFound bool
+}
+
+func Do(f kubeutil.Factory, o GetOpts, args ...string) ([]*unstructured.Unstructured, error) {
+	if o.ChunkSize <= 0 {
+		o.ChunkSize = kubeutil.DefaultChunkSize
+	}
+
+	objs := []*unstructured.Unstructured{}
+
+	r := f.NewBuilder().
+		Unstructured().
+		NamespaceParam(o.Namespace).DefaultNamespace().AllNamespaces(o.AllNamespaces).
+		//FilenameParam(o.ExplicitNamespace, &o.FilenameOptions).
+		LabelSelectorParam(o.LabelSelector).
+		FieldSelectorParam(o.FieldSelector).
+		Subresource(o.Subresource).
+		RequestChunksOf(o.ChunkSize).
+		ResourceTypeOrNameArgs(true, args...).
+		ContinueOnError().
+		Latest().
+		Flatten().
+		Do()
+
+	if o.IgnoreNotFound {
+		r.IgnoreErrors(apierrors.IsNotFound)
+	}
+	if err := r.Err(); err != nil {
+		return objs, err
+	}
+
+	infos, err := r.Infos()
+	if err != nil {
+		return objs, err
+	}
+
+	for _, info := range infos {
+		objs = append(objs, info.Object.(*unstructured.Unstructured))
+	}
+
+	return objs, nil
+}
---
+++ util/helpers.go
@@ -0,0 +1,5 @@
+package util
+
+const (
+	DefaultChunkSize = 500
+)
